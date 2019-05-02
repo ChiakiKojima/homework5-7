55c8a19e243709146a5e9285580bfee2f6af7cff
ChiakiKojima <chiaki.k.cozy@gmail.com>
2019-05-02 08:07:49 +0000

    項目 購入可否追加

--- onlineshopping/index.php
+++ onlineshopping/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head><meta charset="utf-8">
+<link rel="stylesheet" href="CSS/style.css">
 <title>商品一覧</title>
 </head>
 
 <body>
     <h1>商品一覧</h1>
-    <p><a href="form.html">商品の新規作成</a></p>
+    <h3><a href="form.html">商品の新規作成</a></h3>
     
 <?php 
 require_once ('PASS/config.php');
@@ -29,12 +30,20 @@ try {
         <th>商品名</th>
         <th>商品説明</th>
         <th>価格</th>
+        <th>購入可否</th>
     </tr>
     <?php foreach ($result as $row): ?>
     <tr>
         <td><?php echo htmlspecialchars($row['name'],ENT_QUOTES, 'UTF-8') ?></td>
         <td><?php echo htmlspecialchars($row['description'],ENT_QUOTES, 'UTF-8') ?></td>
         <td><?php echo htmlspecialchars($row['price'],ENT_QUOTES, 'UTF-8') ?>円</td>
+        <td>
+        <?php if($row['available'] === 'true'): ?>
+        o
+        <?php else: ?>
+        x
+        <?php endif ?>
+        </td>
         <td><a href="edit.php?id=<?php echo htmlspecialchars($row['id'],ENT_QUOTES, 'UTF-8') ?>">変更</a>
         | <a href="delete.php?name=<?php echo htmlspecialchars($row['name'],ENT_QUOTES, 'UTF-8') ?>">削除</a></td>
     </tr>
